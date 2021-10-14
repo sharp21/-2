9a97785b782e5f7832a9f6c3257e58c001bc9ab9
sharp21 <92368817+sharp21@users.noreply.github.com>
2021-10-14 15:52:13 +0800

    Update TestCaseReadFromExcel.py

--- TestCaseReadFromExcel.py
+++ TestCaseReadFromExcel.py
@@ -11,25 +11,25 @@
 #     def readCell(self, sheet, row, column, defaultValue):
 #         value = sheet.cell(row=row, column=column).value
 #         if value is None:
-#             value = defaultValue
-#         return value
+value = defaultValue
+      return value
 #
 #     def readTestCase(self):
 #         wb = openpyxl.load_workbook(self.fileName)
 #         sheets = wb.sheetnames
 #         models = []
+add 1111111111
+1213312313213
 #         for i in sheets:
 #             sheet = wb.get_sheet_by_name(i)
 #             model = TestModel(sheet.title)
 #             for row in range(2, sheet.max_row + 1):
 #                 name = self.readCell(sheet, row, 1, '-')
 #                 url = self.readCell(sheet, row, 2, '')
-#                 method = self.readCell(sheet, row, 3, '')
+#                 method = self.read, row, 3, '')
 #                 inputParams = self.readCell(sheet, row, 4, '')
 #                 checkMode = self.readCell(sheet, row, 5, '')
-#                 checkUrl = self.readCell(sheet, row, 6, '')
-#                 checkMethod = self.readCell(sheet, row, 7, '')
-#                 checkInputParams = self.readCell(sheet, row, 8, '')
+#                 checkUrl = self.readCell(sheet, 
 #                 checkPoint = self.readCell(sheet, row, 9, '')
 #                 data = TestData(name, url, method, inputParams)
 #                 data.setCheckParam(checkMode, checkUrl, checkMethod, checkInputParams, checkPoint)
